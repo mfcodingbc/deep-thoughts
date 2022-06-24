0616f1318e9d31ab26421b4a7387bbfe9f954387
Mitchell Frey <mfcodingbc@gmail.com>
2022-06-24 00:03:22 -0400

    data on pages, routes setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+// Apollo Client components
 import {
   ApolloProvider,
   ApolloClient,
@@ -7,9 +8,21 @@ import {
   createHttpLink
 } from '@apollo/client';
 
+// React Router components
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+// components of page
 import Header from './components/Header';
 import Footer from './components/Footer';
 
+// other "pages"
+import Login from './pages/Login';
+import NoMatch from './pages/NoMatch';
+import SingleThought from './pages/SingleThought';
+import Profile from './pages/Profile';
+import Signup from './pages/Signup';
+
+// Home page
 import Home from './pages/Home';
 
 const httpLink = createHttpLink({
@@ -25,13 +38,25 @@ const client = new ApolloClient({
 function App() {
   return (
     <ApolloProvider client={client}>
-      <div className='flex-column justify-flex-start min-100-vh'>
-        <Header />
-        <div className='container'>
-          <Home />
+      <Router>
+        <div className='flex-column justify-flex-start min-100-vh'>
+          <Header />
+          <div className='container'>
+            <Routes>
+              <Route path='/' element={<Home />} />
+              <Route path='/login' element={<Login />} />
+              <Route path='/signup' element={<Signup />} />
+              <Route path='/profile'>
+                <Route path=':username' element={<Profile />} />
+                <Route path='' element={<Profile />} />
+              </Route>
+              <Route path='/thought/:id' element={<SingleThought />} />
+              <Route path='*' element={<NoMatch />} />
+            </Routes>
+          </div>
+          <Footer />
         </div>
-        <Footer />
-      </div>
+      </Router>
     </ApolloProvider>
   );
 }
